df804dae99686abb9e0c297e0968967c57d6821b
Cyrus Ingraham <cyrus.ingraham@jamf.com>
2018-09-21 14:02:17 -0500

    Moved contents from internal repository

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# PPPC-Utility
+# ![alt text][logo] Privacy Preferences Policy Control (PPPC) Utility
+
+[logo]: /Resources/Assets.xcassets/AppIcon.appiconset/PPPC_Logo_32%402x.png "PPPC Utility"
+
+PPPC Utility is a macOS application (versions 10.13 and newer) for creating configuration profiles containing the 
+Privacy Preferences Policy Control payload for macOS. The profiles can be saved locally signed or unsigned. 
+Profiles can also be uploaded directly to a Jamf Pro server. 
+
+## Installation
+
+#### [Download the latest version here](https://github.com/jamf/PPPC-Utility/releases)
+
+## Building profile
+Start by adding the bundles/executables for the payload by using drag-and-drop or by selecting the add (+)
+button in the left corner.
+![alt text](/Images/Building.png "Building profile")
+
+## Saving
+Profiles can be saved locally either signed or unsigned.  
+
+![alt text](/Images/SavingUnsigned.png "Building profile")
+
+
+## Upload to Jamf Pro
+
+#### Jamf Pro 10.7.1 and newer
+Starting in Jamf Pro 10.7.1 the Privacy Preferences Policy Control Payload can be uploaded to the API without being signed before uploading.
+![alt text](/Images/UploadUnsigned.png "Upload unsigned")
+
+#### Jamf Pro 10.7.0 and below 
+To upload the Privacy Preferences Policy Control Payload to Jamf Pro 10.7.0 and below, 
+the profile will need to be signed before uploading.
+![alt text](/Images/UploadSigned.png "Upload signed")
+
+
---
+++ Source/View Controllers/OpenViewController.swift
@@ -0,0 +1,77 @@
+//
+//  OpenViewController.swift
+//  PPPC Utility
+//
+//  MIT License
+//
+//  Copyright (c) 2018 Jamf Software
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Cocoa
+
+class OpenViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+    
+    var completionBlock: (([Executable]) -> Void)?
+    
+    var observers: [NSKeyValueObservation] = []
+    
+    @objc dynamic var current: Executable?
+    @objc dynamic var choices: [Executable] = []
+    
+    @IBOutlet var choicesAC: NSArrayController!
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        //  Reload executables
+        current = Model.shared.current
+        if let value = current {
+            choices = Model.shared.getAppleEventChoices(executable: value)
+        }
+    }
+    
+    func tableView(_ tableView: NSTableView, selectionIndexesForProposedSelection proposedSelectionIndexes: IndexSet) -> IndexSet {
+        DispatchQueue.main.async {
+            guard let index = proposedSelectionIndexes.first else { return }
+            self.completionBlock?([self.choices[index]])
+            self.dismiss(self)
+        }
+        return proposedSelectionIndexes
+    }
+
+    @IBAction func prompt(_ sender: NSButton) {
+        let block = completionBlock
+        let panel = NSOpenPanel()
+        panel.allowsMultipleSelection = true
+        panel.allowedFileTypes = [ kUTTypeBundle, kUTTypeExecutable ] as [String]
+        panel.directoryURL = URL(fileURLWithPath: "/Applications", isDirectory: true)
+        panel.begin { response in
+            if response == .OK {
+                var selections : [Executable] = []
+                panel.urls.forEach {
+                    guard let executable = Model.shared.loadExecutable(url: $0) else { return }
+                    selections.append(executable)
+                }
+                block?(selections)
+            }
+        }
+    }
+
+}
