62eed64d867ee5bc64995057c5f72d8da8883c46
Tony Eichelberger <watkyn@gmail.com>
2020-09-14 13:57:06 -0500

    Update Source/Model/Alert.swift
    Co-authored-by: Krzysztof Kot <krzykot@gmail.com>

--- Source/Model/Alert.swift
+++ Source/Model/Alert.swift
@@ -4,7 +4,7 @@
 //
 //  MIT License
 //
-//  Copyright (c) 2018 Jamf Software
+//  Copyright (c) 2020 Jamf Software
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
